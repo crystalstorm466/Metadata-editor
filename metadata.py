d9b8d73c689ebe3ffa62ca8c7f58e938596c0a9b
David Holland <DcHolland_89@msn.com>
2022-01-25 11:04:16 -0500

    This should work now

---
+++ metadata.py
@@ -0,0 +1,22 @@
+import mutagen
+from mutagen import *
+from os import system, name
+
+print("Welcome to Audio Metadata editor\n")
+filepath = input("Please provide the file path to your audio file\n")
+
+audio = mutagen.File(filepath)
+
+WhichOne = input(encoding=3, text=[u"What tag do you want to edit in the file?\nTitle\nArtist\nTrackNumber\nAlbum\nGenre\n"])
+
+if WhichOne == "Title":
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+    NewTitle = input("What is the new title?\n")
+    audio["title"] = NewTitle #mutagen.TextFrame(encoding=3, text=[NewTitle]) 
+    print(audio)
+    mutagen.save(audio)
+
+
