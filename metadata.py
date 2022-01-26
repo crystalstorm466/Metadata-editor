9b61289daf714af6b85e5b985cfcae172eecc64f
David Holland <DcHolland_89@msn.com>
2022-01-26 09:39:50 -0500

    now the Title if statement works after not working

--- metadata.py
+++ metadata.py
@@ -1,3 +1,4 @@
+import unicodedata
 import mutagen
 from mutagen import *
 from os import system, name
@@ -8,14 +9,14 @@ filepath = input("Please provide the file path to your audio file\n")
 audio = mutagen.File(filepath)
 
 WhichOne = input("What tag do you want to edit in the file?\nTitle\nArtist\nTrackNumber\nAlbum\nGenre\n")
-
+print("The answer is " + WhichOne)
 if WhichOne == "Title":
     if name == 'nt':
         system('cls')
     else:
         system('clear')
     NewTitle = input("What is the new title?\n")
-    audio["title"] = NewTitle #mutagen.TextFrame(encoding=3, text=[NewTitle]) 
+    audio["title"] = (NewTitle) #mutagen.TextFrame(encoding=3, text=[NewTitle]) 
     print(audio)
     mutagen.save(audio)
 
