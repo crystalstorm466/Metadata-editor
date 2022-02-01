78acc40dad1193769ae1e5a04a4ebdead3e760e4
David Holland <DcHolland_89@msn.com>
2022-02-01 10:44:46 -0500

    Made it Public

--- metadata.py
+++ metadata.py
@@ -1,4 +1,6 @@
-from re import U
+import sys
+from time import sleep
+import time
 import unicodedata
 import mutagen
 from mutagen import *
@@ -14,10 +16,22 @@ from mutagen.smf import SMF
 from mutagen.easyid3 import EasyID3
 from os import system, name
 
+def Choice2Exit():
+    Exit = input("Do you want to continue editing? \n")
+    if Exit == "Yes" or "Y":
+     TagEdit(filepath, audio)
+    elif Exit == "No" or "N":
+        sys.exit(0)
+
 
 print("Welcome to Audio Metadata editor\n")
 print("Supported File Types are:\nFlAC, MP3, AAC, M4A, OGG,and WAV\n")
+
 filepath = input("Please provide the file path to your audio file\n")
+ 
+
+
+
 
 #this identifes the filetype and opens it, yes I could just use mutagen.File however it does not allow for tag editing
 try: 
@@ -27,28 +41,64 @@ except ID3NoHeaderError:
     audio.add_tags()
 
 
-WhichOne = input("What tag do you want to edit in the file?\nTitle\nArtist\nTrack Number\nAlbum\nGenre\n")
-print("The answer is " + WhichOne)
-if name == 'nt':
-    system('cls') # windows clear command
-else:
-    system('clear') # linux and unix clear command
-if WhichOne == "Title":
-    NewTitle = input("What is the new title?\n")
-    audio["title"] = NewTitle
-    audio.save(v1=0, v2_version=3)
-    print(audio)
-elif WhichOne == "Artist":
-    NewArtist = input("What is the new Artist?")
-    audio["artist"] = NewArtist
-    print(audio)
-    audio.save(v1=0, v2_version=3)
-elif WhichOne == "TrackNumber" or "Track Number":
-    NewTrackNumber = input("What is the Track Number")
-    audio["tracknumber"] = NewTrackNumber
-    print("The new Track Number is: " + audio["tracknumber"])
-    audio.save(v1=0, v2_version=3)
 
 
 
 
+
+def TagEdit(filepath, audio):
+    WhichOne = input("What tag do you want to edit in the file?\nTitle\nArtist\nTrack Number\nAlbum\nGenre\n")
+
+    print("The answer is " + WhichOne)
+    if name == 'nt':
+        system('cls') # windows clear command
+    else:
+        system('clear') # linux and unix clear command
+
+    if WhichOne == "title" or "Title":
+
+        NewTitle = input("What is the new title?\n")
+        audio["title"] = NewTitle
+        audio.save(v1=0, v2_version=3)
+        print(audio)
+
+    elif WhichOne == "Artist" or "artist":
+
+        NewArtist = input("What is the new Artist?")
+        audio["artist"] = NewArtist
+        print(audio)
+        audio.save(v1=0, v2_version=3)
+
+    elif WhichOne == "trackNumber" or "Track Number":
+
+        NewTrackNumber = input("What is the Track Number")
+        audio["tracknumber"] = NewTrackNumber
+        audio.save(v1=0, v2_version=3)
+        print(audio)
+
+    elif WhichOne == "album" or "Album":
+
+        NewAlbum = input("What is the Album name?")
+        audio["album"] = NewAlbum
+        audio.save(v1=0, v2_version=3)
+        print(audio)
+    elif WhichOne == "genre" or "Genre":
+
+        NewGenre = input("What is the Genre?")
+        audio["genre"] = NewGenre
+        audio.save(v1=0, v2_version=3)
+        print(audio)
+
+    time.sleep(3)
+    if name == 'nt':
+        system('cls') # windows clear command
+    else:
+        system('clear') # linux and unix clear command
+    Choice2Exit()
+    
+
+
+TagEdit(filepath, audio)
+
+
+        
